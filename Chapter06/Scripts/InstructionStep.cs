6ca774a77cb6a8e3d951aa90e1e9e04faf30e1fa
jash27 <30648183+jash27@users.noreply.github.com>
2017-10-05 11:44:00 +0530

    Code files added

---
+++ Chapter06/Scripts/InstructionStep.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionStep {
+    public string Name;
+    public string Title;
+    public string BodyText;
+    public string ImageName;
+    public string VideoName;
+
+    private const int NameColumn = 0;
+    private const int TitleColumn = 1;
+    private const int BodyColumn = 2;
+    private const int ImageColumn = 3;
+    private const int VideoColumn = 4;
+
+    public InstructionStep(List<string> values) {
+        foreach (string item in values) {
+            if (values.IndexOf(item) == NameColumn) {
+                Name = item;
+            }
+            if (values.IndexOf(item) == TitleColumn) {
+                Title = item;
+            }
+            if (values.IndexOf(item) == BodyColumn) {
+                BodyText = item;
+            }
+            if (values.IndexOf(item) == ImageColumn) {
+                ImageName = item;
+            }
+            if (values.IndexOf(item) == VideoColumn) {
+                VideoName = item;
+            }
+
+        }
+    }
+
+}
+
---
+++ Chapter06/Scripts/InstructionsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class InstructionEvent : UnityEvent<InstructionStep> { }
+
+public class InstructionsController : MonoBehaviour {
+    public InstructionEvent OnInstructionUpdate = new InstructionEvent();
+
+    private int currentStep;
+    private InstructionModel currentInstructionModel = new InstructionModel();
+
+    void Awake() {
+        currentInstructionModel.LoadData();
+    }
+
+    void Start() {
+        currentStep = 0;
+        CurrentInstructionUpdate();
+    }
+
+    public void NextStep() {
+        if (currentStep < currentInstructionModel.GetCount() - 1) {
+            currentStep++;
+            CurrentInstructionUpdate();
+        }
+    }
+
+    public void PreviousStep() {
+        if (currentStep > 0) {
+            currentStep--;
+            CurrentInstructionUpdate();
+        }
+    }
+
+    private void CurrentInstructionUpdate() {
+        InstructionStep step = currentInstructionModel.GetInstructionStep(currentStep);
+        OnInstructionUpdate.Invoke(step);
+    }
+
+}
